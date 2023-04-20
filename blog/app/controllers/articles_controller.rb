b0a0d941e5f0c2679b666cdb1aa26503fe7e01b0
Hegnon <hegnondutra@gmail.com>
2023-04-20 14:43:54 -0300

    criando article

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -24,16 +24,14 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    
+    if @article.save
+      redirect_to @article      
+    else
+      render :new    
     end
   end
+
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
@@ -66,6 +64,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.fetch(:article, {})
+      params.require(:article).permit(:title, :body)
     end
 end
