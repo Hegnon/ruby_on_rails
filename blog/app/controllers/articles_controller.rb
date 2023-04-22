2a2b5fa3fb9500710d557814b35f32ee0fd8262d
Hegnon <hegnondutra@gmail.com>
2023-04-22 17:37:27 -0300

    Adicionado messagem de erro

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  
 
   # GET /articles or /articles.json
   def index
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article      
     else
-      render :new    
+      render :new, status: :unprocessable_entity   
     end
   end
 
@@ -57,13 +57,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+      
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
