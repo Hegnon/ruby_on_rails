aebfef8641f560c1d5c8870291347d9d38deb722
Hegnon <hegnondutra@gmail.com>
2023-04-23 09:48:07 -0300

    finalizando o crud

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: %i[show edit update destroy]
 
   # GET /articles or /articles.json
   def index
@@ -7,8 +7,7 @@ class ArticlesController < ApplicationController
   end
 
   # GET /articles/1 or /articles/1.json
-  def show
-    @article = Article.find(params[:id])
+  def show    
   end
 
   # GET /articles/new
@@ -17,14 +16,12 @@ class ArticlesController < ApplicationController
   end
 
   # GET /articles/1/edit
-  def edit
-    @article = Article.find(params[:id])
+  def edit    
   end
 
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-
     
     if @article.save
       redirect_to @article      
@@ -35,9 +32,7 @@ class ArticlesController < ApplicationController
 
 
   # PATCH/PUT /articles/1 or /articles/1.json
-  def update
-    @article = Article.find(params[:id])
-
+  def update    
     if @article.update(article_params)
       redirect_to @article      
     else
@@ -47,14 +42,19 @@ class ArticlesController < ApplicationController
       
     
   # DELETE /articles/1 or /articles/1.json
-  def destroy
-    @article = Article.find(params[:id])  
-    @article.destroy    
+  def destroy     
+    @article.destroy 
+    
+    redirect_to root_path
   end
 
   private
       
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])  
   end
 end
