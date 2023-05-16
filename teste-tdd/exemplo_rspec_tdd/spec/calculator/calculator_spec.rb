e92951c1558a49b5cf22fd7215d68e4353efb0b3
Hegnon <hegnondutra@gmail.com>
2023-05-15 21:01:51 -0300

    Adicionado um novo teste

--- teste-tdd/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ teste-tdd/exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,9 +1,15 @@
 require 'calculator'
 
 describe Calculator do
-    it 'metodo soma para 2 numeros'do
+    it 'metodo soma para 2 numeros' do
         calc = Calculator.new
         result = calc.sum(5,7)
         expect(result).to eql(12)  
     end    
+
+    it 'metodo soma para 2 numeros com numeros negativos' do
+        calc = Calculator.new
+        result = calc.sum(-5, 7)
+        expect(result).to eql(2)  
+    end   
 end
